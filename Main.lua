c2a0cf482a39f08b076a295329a53440ada19fdc
unknown <oleundrum@hotmail.com>
2021-02-27 02:12:57 +0100

    Added C#

--- Main.lua
+++ Main.lua
@@ -8,30 +8,93 @@ local Not_OKColor = "#000000"
 local Not_OK_R = 0
 local Not_OK_G = 0
 local Not_OK_B = 0
+
+local Var1_Color = "008C00"
+local Var1_R = .35
+local Var1_G = .55
+local Var1_B = .80
+
+---for skinning--
+local prevtarget = nil
+
+-----------------
 --
 
 ------
+
+local frame_Combat = CreateFrame("Frame",ShowUF,UIParent)
+local CombatTexture = frame_Combat:CreateTexture()
+CombatTexture:SetPoint("TOPLEFT", relativeframe)--set til 1,0 isteden for 4,0
+CombatTexture:SetSize(1,1)-- sett til 1,1 istedenfor 4,4
+CombatTexture:SetTexture("Interface\\AddOns\\WoWUI\\RinornsStats\\BG.png")
+frame_Combat.texture = CombatTexture
+CombatTexture:SetVertexColor(0,0,0)
+frame_Combat:SetSize(256,64)
+frame_Combat:Show()
+
 local frame_Target = CreateFrame("Frame",ShowUF,UIParent)
-local ufTexture = frame_Target:CreateTexture()
-ufTexture:SetPoint("TOPLEFT", relativeframe)
-ufTexture:SetSize(4,4)
-ufTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
-frame_Target.texture = ufTexture
-ufTexture:SetVertexColor(0,0,0)
+local TargetTexture = frame_Target:CreateTexture()
+TargetTexture:SetPoint("TOPLEFT", relativeframe,1,0)
+TargetTexture:SetSize(1,1)
+TargetTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
+frame_Target.texture = TargetTexture
+TargetTexture:SetVertexColor(0,0,0)
 frame_Target:SetSize(256,64)
 frame_Target:Show()
 
+local frame_Range = CreateFrame("Frame",ShowUF,UIParent)
+local RangeTexture = frame_Range:CreateTexture()
+RangeTexture:SetPoint("TOPLEFT", relativeframe,2,0)
+RangeTexture:SetSize(1,1)
+RangeTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
+frame_Range.texture = RangeTexture
+RangeTexture:SetVertexColor(0,0,0)
+frame_Range:SetSize(256,64)
+frame_Range:Show()
 
-local frame_Combat = CreateFrame("Frame",ShowUF,UIParent)
+-------------//MovementCordPixels//----------
+local frame_XCord = CreateFrame("Frame",ShowUF,UIParent)
+local XCordTexture = frame_XCord:CreateTexture()
+XCordTexture:SetPoint("TOPLEFT", relativeframe,0,1)
+XCordTexture:SetSize(10,10)
+XCordTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
+frame_XCord.texture = XCordTexture
+XCordTexture:SetVertexColor(0,0,0)
+frame_XCord:SetSize(256,64)
+frame_XCord:Show()
 
-local ufTexture = frame_Combat:CreateTexture()
-ufTexture:SetPoint("TOPLEFT", relativeframe,4,0)
-ufTexture:SetSize(4,4)
-ufTexture:SetTexture("Interface\\AddOns\\WoWUI\\RinornsStats\\BG.png")
-frame_Combat.texture = ufTexture
-ufTexture:SetVertexColor(0,0,0)
-frame_Combat:SetSize(256,64)
-frame_Combat:Show()
+local frame_XDesiCord = CreateFrame("Frame",ShowUF,UIParent)
+local XDesiCordTexture = frame_XDesiCord:CreateTexture()
+XDesiCordTexture:SetPoint("TOPLEFT", relativeframe,0,2)
+XDesiCordTexture:SetSize(1,1)
+XDesiCordTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
+frame_XDesiCord.texture = XDesiCordTexture
+XDesiCordTexture:SetVertexColor(0,0,0)
+frame_XDesiCord:SetSize(256,64)
+frame_XDesiCord:Show()
+
+local frame_YCord = CreateFrame("Frame",ShowUF,UIParent)
+local YCordTexture = frame_YCord:CreateTexture()
+YCordTexture:SetPoint("TOPLEFT", relativeframe,0,1)
+YCordTexture:SetSize(1,1)
+YCordTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
+frame_YCord.texture = YCordTexture
+YCordTexture:SetVertexColor(0,0,0)
+frame_YCord:SetSize(256,64)
+frame_YCord:Show()
+
+local frame_YDesiCord = CreateFrame("Frame",ShowUF,UIParent)
+local YDesiCordTexture = frame_YDesiCord:CreateTexture()
+YDesiCordTexture:SetPoint("TOPLEFT", relativeframe,0,2)
+YDesiCordTexture:SetSize(1,1)
+YDesiCordTexture:SetTexture("Interface\\AddOns\\WoWUI\\Background\\NaTUI")
+frame_YDesiCord.texture = YDesiCordTexture
+YDesiCordTexture:SetVertexColor(0,0,0)
+frame_YDesiCord:SetSize(256,64)
+frame_YDesiCord:Show()
+
+
+-------------//MovementCordPixels//----------
 
 ------
 local frame = CreateFrame("FRAME", "FooAddonFrame");
@@ -39,8 +102,8 @@ local TestFrame = CreateFrame("FRAME", "FooAddonFrame");
 local screenWidth = GetScreenWidth()
 print(screenWidth);
 --Events
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:RegisterEvent("UNIT_HEALTH");
+frame:RegisterEvent("UNIT_COMBAT");
 frame:RegisterEvent("PLAYER_REGEN_DISABLED")
 frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 --target--
@@ -48,47 +111,54 @@ frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 --target--
 
 frame:SetScript("OnEvent", function(self, event)
-	if(event == "PLAYER_ENTERING_WORLD") then
-		print("Hello World! Hello " .. event..UnitName("Player"));
-	end
 	if(event == "UNIT_HEALTH") then
 		print(event.. "Current Health " ..UnitHealth("player"));
 	end
+	if(event == "UNIT_COMBAT") then
+		if(IsActionInRange(12)) then
+			RangeTexture:SetVertexColor(OK_R,OK_G,OK_B)
+		elseif(IsActionInRange(11))	then
+			RangeTexture:SetVertexColor(Var1_R,Var1_G,Var1_B)
+		end
+	end		
+	--COMBAT--
 	if(event == "PLAYER_REGEN_DISABLED") then
-		print("Entered combat" .. event);
+		frame_Combat.texture = CombatTexture
+		CombatTexture:SetVertexColor(OK_R,OK_G,OK_B)
+		frame_Combat:Show()
 	end
 	if(event == "PLAYER_REGEN_ENABLED") then
-		print("Left combat" .. event);
+		CombatTexture:SetVertexColor(Not_OK_R,Not_OK_G,Not_OK_B)
+		RangeTexture:SetVertexColor(Not_OK_R,Not_OK_G,Not_OK_B)
 	end
-	
+	--COMBAT--
 	--TARGET Functionality
 	if(event == "PLAYER_TARGET_CHANGED") then
 		--if target exists
 		if(UnitExists("target")) then
+			myTarget = "target"
 			print("Changed target to " ..UnitName("target"));
 			--if the target is killable target	
 			if not(UnitIsFriend("Player","target")) then 
 				--if target is alive
 				if not(UnitIsDead("target")) then
-					frame_Target.texture = ufTexture
-					ufTexture:SetVertexColor(OK_R,OK_G,OK_B)
-					frame_Target:Show()
+					TargetTexture:SetVertexColor(OK_R,OK_G,OK_B)
+					if(IsActionInRange(12)) then
+						RangeTexture:SetVertexColor(OK_R,OK_G,OK_B)
+					elseif(IsActionInRange(11) and CheckInteractDistance("target", 1))	then
+						RangeTexture:SetVertexColor(Var1_R,Var1_G,Var1_B)
+					else
+						RangeTexture:SetVertexColor(Not_OK_R,Not_OK_G,Not_OK_B)
+					end
 				else --target is lootable/skinnable 
-					frame_Target.texture = ufTexture
-					ufTexture:SetVertexColor(.35,.55,.80)
-					frame_Target:Show()	
+					TargetTexture:SetVertexColor(.35,.55,.80)
 				end
 			end	
 		else 
-			frame_Target.texture = ufTexture
-			ufTexture:SetVertexColor(Not_OK_R,Not_OK_G,Not_OK_B)
+			frame_Target.texture = TargetTexture
+			TargetTexture:SetVertexColor(Not_OK_R,Not_OK_G,Not_OK_B)
 			frame_Target:Show()
 		end
 	end
-	
 end)
-
-
-
-
 
---
+++ RinorsStatistics/RinorsStatistics/MainWindow.xaml.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Color = System.Drawing.Color;
+using MessageBox = System.Windows.MessageBox;
+
+namespace RinorsStatistics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+        Process pros;
+        bool HadTarget = false;
+
+    #region states
+        private enum ProgramState
+        {
+            ACTIVE,
+            INACTIVE
+        }
+        private ProgramState currentProgramState;
+
+        private enum CurrentSelectedClass
+        {
+            NONE,
+            HUNTER,
+            MAGE,
+            WARLOCK,
+            PRIEST,
+            DRUID,
+            WARRIOR,
+            ROGUE,
+            PALADIN
+        }
+        private CurrentSelectedClass currentClass;
+
+        private enum CharacterState
+        {
+            NONE,
+            ALIVE,
+            DEAD            
+        }
+        private CharacterState currentCharacterState;
+
+        private enum ActionState
+        {
+            MOVING,
+            STANDING,
+            DRINKING,
+            FIGHTING,
+            LOOTING,
+            SKINNING
+        }
+        private ActionState currentActionState;
+
+        private enum TargetState
+        {            
+            NOTARGET,
+            DEADTARGET,
+            KILLABLETARGET
+        }
+        private TargetState currentTargetState;
+
+        private enum RangeState
+        {
+            NONE,
+            OUTOFRANGE,
+            MELLE,
+            RANGED
+        }
+        private RangeState currentRangeState;
+
+        private enum PetState
+        {
+            NONE,
+            NOPET,
+            ALIVE,
+            DEAD
+        }
+        private PetState currentPetState;
+
+        private enum PetMiscState
+        {
+            NOACTIONREQ,
+            ACTIONREQ
+        }
+        private PetMiscState currentPetMiscState;
+        #endregion
+
+    #region Keys
+        const uint WM_KEYDOWN = 0x100;
+        const uint WM_KEYUP = 0x101;
+        const int VK_W = 0x57;
+        const int VK_TAB = 0x09;
+        const int VK_Q = 0x51;
+        const int VK_E = 0x45;
+        const int VK_K = 0x4B;
+        const int VK_S = 0x53;
+        const int VK_L = 0x4C;
+
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartProcess();
+            
+        }
+
+        private void StartProcess()
+        {
+            pros = Process.Start(@"E:\Games\World of Warcraft\_classic_\WowClassic.exe");
+        }
+
+        private void SetStartStates()
+        {
+            currentProgramState = ProgramState.INACTIVE;
+            currentClass = CurrentSelectedClass.NONE;
+            currentActionState = ActionState.STANDING;
+            currentCharacterState = CharacterState.NONE;
+            currentPetMiscState = PetMiscState.NOACTIONREQ;
+            currentPetState = PetState.NONE;
+            currentRangeState = RangeState.NONE;
+            currentTargetState = TargetState.NOTARGET;
+        }
+
+        private void OnButtonStartProgram(object sender, RoutedEventArgs e)
+        {
+            SetStartStates();
+            ProgramLoop();
+        }
+
+        private void CheckTargetState()
+        {
+            if (SearchPixel("#00B000", 1, 0))
+            {
+                //MessageBox.Show("Has target");
+                currentTargetState = TargetState.KILLABLETARGET;
+            }
+            else if(SearchPixel("#008C00", 1, 0))
+            {
+                //MessageBox.Show("TargetDead");
+                currentTargetState = TargetState.DEADTARGET;
+            }
+            else
+            {
+                //MessageBox.Show("No target");
+                currentTargetState = TargetState.NOTARGET;
+            }
+        }
+
+        private void CheckRangeState()
+        {
+            if (SearchPixel("#00B000", 2, 0))
+            {
+                //MessageBox.Show("Has target");
+                currentRangeState = RangeState.RANGED;
+            }
+            else if (SearchPixel("#008C00", 2, 0))
+            {
+                //MessageBox.Show("TargetDead");
+                currentRangeState = RangeState.MELLE;
+            }
+            else
+            {
+                //MessageBox.Show("No target");
+                currentRangeState = RangeState.OUTOFRANGE;
+            }
+        }
+
+
+        private void ProgramLoop()
+        {
+            CheckTargetState();
+            if (currentTargetState == TargetState.NOTARGET)
+            {   
+                //tabs after a target
+                DoAction(VK_TAB);
+                Thread.Sleep(333);
+                CheckTargetState();
+                if (currentTargetState == TargetState.KILLABLETARGET)
+                {
+                    DoAction(VK_K);
+                    currentActionState = ActionState.FIGHTING;
+                    CombatLoop();
+                }
+            }
+        }
+
+
+
+        private void DoAction(int key)
+        {
+            SetForegroundWindow(pros.MainWindowHandle);
+            PostMessage(pros.MainWindowHandle, WM_KEYDOWN, key, 0);
+            PostMessage(pros.MainWindowHandle, WM_KEYUP, key, 0);
+
+        }
+
+        private void CombatLoop()
+        {
+            CheckTargetState();
+            if (currentTargetState == TargetState.KILLABLETARGET)
+            {
+                HadTarget = true;
+                CheckRangeState();
+                if (currentRangeState == RangeState.OUTOFRANGE)
+                {
+                    DoAction(VK_K);
+                    DoAction(VK_Q);
+                }
+                else if (currentRangeState == RangeState.RANGED)
+                {
+                    DoAction(VK_Q);
+                }
+                else if (currentRangeState == RangeState.MELLE)
+                {
+                    DoAction(VK_E);
+                }
+                Random rnd = new Random();
+                Thread.Sleep(rnd.Next(300, 2000));
+
+                CombatLoop();
+            }
+            else if (currentTargetState == TargetState.NOTARGET && HadTarget)
+            {
+                HadTarget = false;
+                currentActionState = ActionState.LOOTING;
+                Random rnd = new Random();
+                
+                DoAction(VK_L);
+                Thread.Sleep(rnd.Next(600, 700));
+                DoAction(VK_K);
+                Thread.Sleep(rnd.Next(5000, 5300));
+                DoAction(VK_K);
+                Thread.Sleep(rnd.Next(1000, 1300));
+                DoAction(VK_K);
+                Thread.Sleep(3500);
+
+            }
+        }
+
+        private bool SearchPixel(string color, int xPos, int yPos)
+        {
+            //bitmap with screen size
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            //object that can capture the screen
+            Graphics graphics = Graphics.FromImage(bitmap as System.Drawing.Image);
+            //Takes the screenshot
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+            Color convertedColor = ColorTranslator.FromHtml(color);
+
+            Color currentPixColor = bitmap.GetPixel(xPos, yPos);
+            if (currentPixColor == convertedColor)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
